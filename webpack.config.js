1ef3323ab2db1b737ce9d85c6a2d228d806c88f6
lan3945663 <lan3945663@gmail.com>
2021-09-21 01:54:39 +0300

    init: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+   entry: './src/index.js',
+   output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  module: {
+    rules: [{
+      test:/\.(s*)css$/,
+      use: [
+        MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader',
+      ]
+    }]
+   },
+   plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'style.css'
+      }),
+      new HtmlWebpackPlugin({
+        minify: false,
+        template: path.resolve(__dirname, './src/index.html'),
+        filename: 'index.html',
+        favicon: './src/assets/favicon.ico',
+        inject: 'body',
+      }),
+      new CleanWebpackPlugin()
+   ]
+};
