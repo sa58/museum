38c03278e7a9ba463bc337a4bfe2deb5bd052733
lan3945663 <lan3945663@gmail.com>
2021-10-04 02:55:09 +0300

    feat: add addapt to modal, pp issues

--- src/script.js
+++ src/script.js
@@ -1,36 +1,3 @@
-console.log(`
-(10) верстка валидная
-(24) Вёрстка семантическая
-(44 - 45) PP header, welcome, visiting, explore, video, gallery, tickets, contacts, footer
-
-(2) форма выдвигается
-(2) форма открывается / закрывается корректно
-(8) все элементы исполььзуются
-(7 - 10) PP форма
-
-(2) favicon
-(2) флексы или гриды
-(2) вёрстка размещается по центру
-(2) фоновый цвет каждого блока и секции тянется на всю ширину страницы
-(2) svg
-(2) letter-spacing
-(2) радиокнопки
-(2) mailto, tel
-(2) соцсети
-
-(5) плавная прокрутка по якорям
-(5) параллакс
-(5) панорамы Google Street View
-(4) изменение стиля интерактивных элементов при наведении и клике
-(2) плавная интерактивность
-(2) интерактивность при наведении карточек в секции Visiting
-(2) интерактивность при наведении иконок социальных сетей в футере
-
-(2) ползунки
-(2) + / -
-(0) ripple-эффект
-(10) картины в блоке Galery отображаются в рандомном порядке
-`)
 
 
 // ------------------------------
@@ -64,16 +31,16 @@ progress1.addEventListener('input', function() {
   this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`
 })
 
-L.mapbox.accessToken = 'pk.eyJ1IjoibGFuMzk0NTY2MyIsImEiOiJja3UwYThsYWQyM3AzMnJxdDh5cDhwMGRnIn0.QZJ4Y2w_sHI2JnimfEqD6g';
-var map = L.mapbox.map('map')
-    .setView([48.86091, 2.3364], 16)
-    .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/light-v10'));
+// L.mapbox.accessToken = 'pk.eyJ1IjoibGFuMzk0NTY2MyIsImEiOiJja3UwYThsYWQyM3AzMnJxdDh5cDhwMGRnIn0.QZJ4Y2w_sHI2JnimfEqD6g';
+// var map = L.mapbox.map('map')
+//     .setView([48.86091, 2.3364], 16)
+//     .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/light-v10'));
     
-    L.marker([48.86091, 2.3364]).addTo(map);
-    L.marker([48.8602, 2.3333]).addTo(map);
-    L.marker([48.8607, 2.3397]).addTo(map);
-    L.marker([48.8619, 2.3330]).addTo(map);
-    L.marker([48.8625, 2.3365]).addTo(map);
+//     L.marker([48.86091, 2.3364]).addTo(map);
+//     L.marker([48.8602, 2.3333]).addTo(map);
+//     L.marker([48.8607, 2.3397]).addTo(map);
+//     L.marker([48.8619, 2.3330]).addTo(map);
+//     L.marker([48.8625, 2.3365]).addTo(map);
 
 
     // --------------------
@@ -105,7 +72,7 @@ modalIconClose.addEventListener('click', () => {
 
 
 
-// ------------------------------
+// // ------------------------------
 
 const pictureInnerContainer = document.querySelector('.picture-inner-container');
 
