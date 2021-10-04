b1ead75c952fa422d6bef5c098e3c383e5a96511
lan3945663 <lan3945663@gmail.com>
2021-10-04 22:23:28 +0300

    fix: fix img paths

--- src/script.js
+++ src/script.js
@@ -125,21 +125,21 @@ modalIconClose.addEventListener('click', () => {
 const pictureInnerContainer = document.querySelector('.picture-inner-container');
 
 const links = [
-  `assets/img/galery/galery1.jpg`,
-  `assets/img/galery/galery2.jpg`,
-  `assets/img/galery/galery3.jpg`,
-  `assets/img/galery/galery4.jpg`,
-  `assets/img/galery/galery5.jpg`,
-  `assets/img/galery/galery6.jpg`,
-  `assets/img/galery/galery7.jpg`,
-  `assets/img/galery/galery8.jpg`,
-  `assets/img/galery/galery9.jpg`,
-  `assets/img/galery/galery10.jpg`,
-  `assets/img/galery/galery11.jpg`,
-  `assets/img/galery/galery12.jpg`,
-  `assets/img/galery/galery13.jpg`,
-  `assets/img/galery/galery14.jpg`,
-  `assets/img/galery/galery15.jpg`
+  `./assets/img/galery/galery1.jpg`,
+  `./assets/img/galery/galery2.jpg`,
+  `./assets/img/galery/galery3.jpg`,
+  `./assets/img/galery/galery4.jpg`,
+  `./assets/img/galery/galery5.jpg`,
+  `./assets/img/galery/galery6.jpg`,
+  `./assets/img/galery/galery7.jpg`,
+  `./assets/img/galery/galery8.jpg`,
+  `./assets/img/galery/galery9.jpg`,
+  `./assets/img/galery/galery10.jpg`,
+  `./assets/img/galery/galery11.jpg`,
+  `./assets/img/galery/galery12.jpg`,
+  `./assets/img/galery/galery13.jpg`,
+  `./assets/img/galery/galery14.jpg`,
+  `./assets/img/galery/galery15.jpg`
 ]
 
 function shuffle(array) {
